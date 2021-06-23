58351dc9024954acf49f53866d192191706c8ed8
beryliy <berillium11@gmail.com>
2021-06-23 21:07:05 +0300

    new-soc with TS ver. 1.0

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,37 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, {useEffect} from 'react';
 import './App.css';
+import Sidebar from './modules/navbar/Sidebar';
+import {Route} from "react-router";
+import Dialogs from "./modules/dialogs/Dialogs";
+import {BrowserRouter} from "react-router-dom";
+import UsersContainer from "./modules/users/UsersContainer";
+import HeaderContainer from './modules/header/HeaderContainer';
+import {Login} from './modules/login/Login';
+import ProfileContainer from './modules/profile/ProfileContainer';
+import { useDispatch } from 'react-redux';
+import { getUserDataTC } from './redux/AuthReducer';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+const App = () => {
+    let dispatch = useDispatch()
+    useEffect(() => {
+        dispatch(getUserDataTC())
+    })
+    return (
+            <BrowserRouter>
+                <div className="app-wrapper">
+                    <HeaderContainer/>
+                    <Sidebar />
+                    <div className="app-wrapper-content">
+                            <Route path="/dialog" render={() => <Dialogs />}
+                            />
+                            <Route path="/profile/:userId?" render={() => <ProfileContainer />}
+                            />
+                            <Route path='/users' render={() => <UsersContainer />} />
+                            <Route path='/login' render={() => <Login />} />
+                    </div>
+                </div>
+            </BrowserRouter>
+    );
 }
 
 export default App;
